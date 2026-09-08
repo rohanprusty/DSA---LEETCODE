63d55db0eeb4fc06cdbef3fec753ddf900ebe0d4
ROHAN PRUSTY <rohankrprusty@gmail.com>
2026-09-08 09:39:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (2.94%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        stack<char> st;
+        for (char c : s) 
+            if (isalpha(c))
+                st.push(c);
+            
+        for (char &c : s) {
+            if (isalpha(c)) {
+                c = st.top();
+                st.pop();
+            }
+        }
+        return s;
+    }
+};
